14c5707df4cc3bc92bdb67e676954f557a7d22cf
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-07-23 14:11:56 +0400

    update

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -29,52 +29,48 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div>
-      <div className="bg-[#f6f2ff] p-5 rounded-2xl sm:w-[360px] w-full shadow">
-        <div className="relative w-full h-[230px]">
+      <div className="bg-[#fbf9ff] p-5 rounded-2xl sm:w-[360px] w-full shadow">
+        <div className="">
+          <div className="flex justify-between items-end">
+            <h3 className="font-semibold text-[20px]">{name}</h3>
+
+            <div className=" inset-0 flex justify-end card-img_hover gap-1">
+              {/* GitHub */}
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <Image
+                  src={github}
+                  alt="github"
+                  className="w-2/3 h-2/3 object-contain"
+                />
+              </div>
+
+              {/* Live */}
+              <div
+                onClick={() => window.open(live_link, "_blank")}
+                className="bg-[#009b2d] w-8 h-8 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <Image
+                  src={live}
+                  alt="live"
+                  className="w-2/3 h-2/3 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/* <p className="mt-2 text-secondary text-[14px]">{description}</p> */}
+        </div>
+        <div className="relative w-full h-[230px] mt-4">
           <Image
             src={image}
             alt={name}
             className="w-full h-full object-cover rounded-2xl"
           />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover gap-1">
-            {/* GitHub */}
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="relative bg-black w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <Image
-                src={github}
-                alt="github"
-                className="w-2/3 h-2/3 object-contain absolute"
-              />
-            </div>
-
-            {/* Live */}
-            <div
-              onClick={() => window.open(live_link, "_blank")}
-              className="bg-[#009b2d] w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <Image
-                src={live}
-                alt="live"
-                className="w-1/2 h-1/2 object-contain"
-              />
-            </div>
-          </div>
         </div>
 
-        <div className="mt-5">
-          <h3 className="font-bold text-[20px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-        <div className="flex flex-wrap gap-2 mt-4">
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
-        </div>
       </div>
     </div>
   );

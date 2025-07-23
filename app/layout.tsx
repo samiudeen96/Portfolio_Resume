071b8790168d1bc1f7766d29e8c59644f50b5191
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-07-23 08:48:35 +0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from "next";
-import { Outfit, Ovo } from "next/font/google";
 import { Poppins  } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
-// export const outfit = Outfit({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "600", "700"],
-//   variable: "--font-outfit",
-// });
-
-// export const ovo = Ovo({
-//   subsets: ["latin"],
-//   weight: ["400"],
-//   variable: "--font-ovo",
-// });
 
 export const poppins = Poppins({
   subsets: ["latin"],

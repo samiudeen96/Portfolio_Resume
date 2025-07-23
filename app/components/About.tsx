9d1a0114ea1f7b8e60c5049718c3138386a3069a
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-07-23 16:37:01 +0400

    update

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -3,21 +3,23 @@ import Title from "./Title";
 
 const About = () => {
   return (
-    <div id="about" className="wrapper scroll-mt-12">
-      <Title text1="Introduction" text2="About Me" />
+    <div className="bg-[#f9f9f9]">
+      <div id="about" className="wrapper scroll-mt-12">
+        <Title text1="Introduction" text2="About Me" />
 
-      <p>
-        A passionate Frontend and Full Stack Developer with over 3 years of
-        professional experience building fast, scalable, and responsive web
-        applications using React.js, Node.js, Express.js, MongoDB, and MySQL. I
-        bring a strong foundation in both frontend architecture and backend API
-        development, ensuring seamless user experiences from end to end.
-      </p>
-      <p className="mt-4">
-        Currently based in Dubai, I&apos;m actively seeking on-site or hybrid
-        opportunities in Dubai or Abu Dhabi, where I can contribute to modern
-        web platforms and collaborate with creative teams.
-      </p>
+        <p>
+          A passionate Frontend and Full Stack Developer with over 3 years of
+          professional experience building fast, scalable, and responsive web
+          applications using React.js, Node.js, Express.js, MongoDB, and MySQL.
+          I bring a strong foundation in both frontend architecture and backend
+          API development, ensuring seamless user experiences from end to end.
+        </p>
+        <p className="mt-4">
+          Currently based in Dubai, I&apos;m actively seeking on-site or hybrid
+          opportunities in Dubai or Abu Dhabi, where I can contribute to modern
+          web platforms and collaborate with creative teams.
+        </p>
+      </div>
     </div>
   );
 };

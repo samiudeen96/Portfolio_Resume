dd950622eee75203e331fc2affa4cffdf1b54e54
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-07-23 10:23:04 +0400

    update

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -64,7 +64,7 @@ const Home = () => {
           <button className="relative p-0 mx-2 border-none bg-transparent cursor-pointer outline-offset-1 transition duration-250 hover:brightness-110">
             <span className="absolute top-0 left-0 w-full h-full rounded-[12px] bg-black/25 shadow-transform transition-all duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] translate-y-[2px] hover:translate-y-[4px] active:translate-y-[1px]"></span>
             <span className="absolute top-0 left-0 w-full h-full rounded-[12px] bg-gradient-to-l from-[#7030e0] via-[#915eff] to-[#7030e0]"></span>
-            <span className="relative flex items-center gap-2 rounded-[12px] px-[27px] py-[8px] text-white text-base bg-[#915eff] translate-y-[-4px] transition-all duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] hover:translate-y-[-6px] hover:duration-[250ms] hover:ease-[cubic-bezier(0.3,0.7,0.4,1.5)] active:translate-y-[-2px] active:duration-[34ms]">
+            <span className="relative flex items-center gap-2 rounded-[12px] px-[27px] py-[8px] text-white text-sm bg-[#915eff] translate-y-[-4px] transition-all duration-[600ms] ease-[cubic-bezier(0.3,0.7,0.4,1)] hover:translate-y-[-6px] hover:duration-[250ms] hover:ease-[cubic-bezier(0.3,0.7,0.4,1.5)] active:translate-y-[-2px] active:duration-[34ms]">
               My Resume <TfiDownload />
             </span>
           </button>

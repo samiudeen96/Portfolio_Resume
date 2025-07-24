c6994d94b238c16adbf50c6aa6d2bb6d0d084f6a
Samiudeen2021 <sami@hasnatech.onmicrosoft.com>
2025-07-24 20:49:09 +0400

    update

--- app/components/common/TechStackCard.tsx
+++ app/components/common/TechStackCard.tsx
@@ -6,7 +6,7 @@ const TechStackCard: React.FC<TechStack> = ({ title, tech }) => {
   return (
     <div className="shadow border-3 border-white rounded-md p-4">
       <h3 className="text-center mb-4 font-semibold text-lg">{title}</h3>
-      <div className="flex gap-2 flex-wrap">
+      <div className="flex gap-2 flex-wrap justify-center">
         {tech.map((item, index: number) => (
           <div key={index} className="flex bg-white gap-2 items-center rounded-full p-[4px]">
             <div className="shadow w-10 h-10 p-2  rounded-full flex justify-center items-center">
